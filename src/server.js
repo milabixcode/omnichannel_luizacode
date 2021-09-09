cf78bccedcd8de091e155093d48cc565fba6f0d8
Douglas Morais <douglas.morais@i-value.com.br>
2021-09-08 21:12:08 -0300

    sucrase

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const app = require('./app.js'); 
+import app from './app.js'; 
 
 
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup())
 
-app.get('/terms', (request, response) => {
-    return response.json({ message: 'Termos de Serviço'});
+// app.get('/terms', (request, response) => {
+//     return response.json({ message: 'Termos de Serviço'});
 
-});
+// });
 
 
-app.listen(3333, () => console.log('Server is running on port 3333'))
+app.listen(3333);
+console.log('Server is running on port 3333')
 
 //const PORT = 3333; 
 //app.listen(PORT);  
